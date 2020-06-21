8b23c605b049ec4b23f13493ae2980f4e989b32a
Trevor <hauck.trevor@gmail.com>
2020-06-20 21:42:01 -0400

    Force ssl

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ const path = require('path')
 const app = require('./server/src/app')
 app.use(express.json())
 
+const forceSSL = (req, res, next) => {
+    if(process.env.NODE_ENV === 'production'){
+        console.log(req.headers)
+        if(req.header('x-forwarded-proto') === 'http') {
+            return res.redirect(301, `https://ancientwisdom.io/`)
+        }
+        if(req.header('host') === 'www.ancientwisdom.io'){
+            return res.redirect(301, 'https://ancientwisdom.io/')
+        }
+    }
+    return next();
+}
+
+app.use(forceSSL)
+
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
 
