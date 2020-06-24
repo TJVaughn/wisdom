aa58145fd8d40111adc8496945f2d93b09df7af1
Trevor <hauck.trevor@gmail.com>
2020-06-24 19:44:31 -0400

    Adding twitter config

--- client/src/modules/Footer.js
+++ client/src/modules/Footer.js
@@ -1,10 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 export default function Footer(){
-    useEffect(() => {
-        
-    })
-
     return (
         <div>
             <p>
