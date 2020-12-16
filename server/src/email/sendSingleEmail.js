9e584601b6e5c3e728317a076dcb1461853e45a2
vaughn <hauck.trevor@gmail.com>
2020-12-15 19:57:47 -0500

    Updated send single email to work just by sending a request to api endpoint.

--- server/src/email/sendSingleEmail.js
+++ server/src/email/sendSingleEmail.js
@@ -30,22 +30,22 @@ module.exports = sendSingleEmail
 
 // Hey there, 
 
-//         <p>Today I am asking a favor of you. There is no obligation or requirement, just an ask from me with hat in hand.
-//         </p>
-//         <p>
-//         As you may know, operating a website involves some small costs, that add up over time. 
-//         So I am asking you, if you are able, to donate $5 to help me cover these costs(hosting, domain...etc).
-//         <a href="https://www.paypal.com/donate?hosted_button_id=LUV97KNZB5PDY">Donate Here</a>
-//         </p>
-//         <p>
+        <p>Today I am asking a favor of you. There is no obligation or requirement, just an ask from me with hat in hand.
+        </p>
+        <p>
+        As you may know, operating a website involves some small costs, that add up over time. 
+        So I am asking you, if you are able, to donate $5 to help me cover these costs(hosting, domain...etc).
+        <a href="https://www.paypal.com/donate?hosted_button_id=LUV97KNZB5PDY">Donate Here</a>
+        </p>
+        <p>
         
-//         If you have gotten that much in value this year, or you just want to help out, I would greatly appreciate the help.
-//         </p>
-//         <p>
-//         If not, I completely understand.
-//         Enjoy the quotes, and if you have any feedback, requests or concerns, you can tweet me @ancientwisbot. 
-//         <a href="https://twitter.com/ancientwisbot">Twitter</a>
-//         </p>
-//         <p>
-//         Thank you and have a wonderful day!
-//         </p>
+        If you have gotten that much in value this year, or you just want to help out, I would greatly appreciate the help.
+        </p>
+        <p>
+        If not, I completely understand.
+        Enjoy the quotes, and if you have any feedback, requests or concerns, you can tweet me @ancientwisbot. 
+        <a href="https://twitter.com/ancientwisbot">Twitter</a>
+        </p>
+        <p>
+        Thank you and have a wonderful day!
+        </p>
